bad7421df0931e10aa75f5a38c9ade346bbac3ea
Dmitriy <dspetrovv@yandex.ru>
2022-06-22 18:48:57 +0300

    sport coaches

---
+++ src/components/cards/CoachCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="card__coach">
+    <img :src="require(`@/assets/images/${photo}.png`)" alt="coach" />
+    <h3 class="card-coach__name">{{ name }}</h3>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent, PropType } from "vue";
+
+export default defineComponent({
+  props: {
+    name: {
+      type: String as PropType<string>,
+      required: true,
+    },
+    photo: {
+      type: String as PropType<string>,
+      required: true,
+    },
+  },
+});
+</script>
